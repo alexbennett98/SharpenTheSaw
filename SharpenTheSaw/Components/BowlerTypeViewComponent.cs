3152fc4cf8c066ea0d83443f74b217ebc1afb348
alex bennett <alexbennett98@gmail.com>
2021-04-17 21:25:27 -0600

    Add project files.

---
+++ SharpenTheSaw/Components/BowlerTypeViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharpenTheSaw.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpenTheSaw.Components
+{
+    public class BowlerTypeViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public BowlerTypeViewComponent(BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
---
+++ SharpenTheSaw/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SharpenTheSaw.Models;
+using SharpenTheSaw.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpenTheSaw.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext ctx)
+        {
+            _logger = logger;
+            context = ctx;
+        }
+
+        public IActionResult Index(long? bowlertypeid, string bowlertype, int pageNum = 0)
+        {
+            int pageSize = 5;
+
+            return View(new IndexViewModel
+            {
+                Bowlers = context.Bowlers
+                .FromSqlInterpolated($"SELECT * from Bowlers WHERE TeamID = {bowlertypeid} OR {bowlertypeid} IS NULL")
+
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToList(),
+
+                PageNumberingInfo = new PageNumberingInfo
+                {
+                    NumItemsPerPage = pageSize,
+                    CurrentPage = pageNum,
+                    //no meal selected get full count, else get count
+                    TotalNumItems = (bowlertypeid == null ? context.Bowlers.Count() :
+                    context.Bowlers.Where(x => x.TeamId == bowlertypeid).Count())
+                },
+
+                TeamCategory = bowlertype
+            }); 
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
